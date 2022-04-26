1a0c6da4101ac4ba6ded4e4940648912c4d0e4c5
Stephen Reish <spreish@ncsu.edu>
2022-04-26 15:42:54 -0400

    More logic...

--- mystery_word.py
+++ mystery_word.py
@@ -11,19 +11,19 @@ def play_game():
         contents_list = contents_string.split()
         # print(contents_list)
         random_word = random.choice(contents_list)
-        print(random_word)
+        # print(random_word)
         print(f'I am thinking of a word. It has {len(random_word)} letters...')
         # Ask the player to guess 8 times.
         guesses = ''
-        turns = 0
+        turns = 8
         while turns > 0:
             failed = 0
             for letter in random_word:
                 if letter in guesses:
                     print(letter, end=" ")
                 else:
-                    print("_")
-                    print(letter, end=" ")
+                    # print("_")
+                    # print(letter, end=" ")
                     failed += 1
             if failed == 0:
                 print(f'Good job! You guessed the word in {turns} guesses!')
