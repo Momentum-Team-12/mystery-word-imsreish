f30fe1d990eb0442fd5250618fe4e35c0a66a570
Stephen Reish <spreish@ncsu.edu>
2022-04-26 15:31:43 -0400

    Changes to logic

--- mystery_word.py
+++ mystery_word.py
@@ -20,16 +20,22 @@ def play_game():
             failed = 0
             for letter in random_word:
                 if letter in guesses:
-                    print(letter, end="")
+                    print(letter, end=" ")
                 else:
                     print("_")
                     print(letter, end=" ")
                     failed += 1
             if failed == 0:
-                print(f'Good job! You guessed the word in ' + {str(random_word)} + ' guesses!')
+                print(f'Good job! You guessed the word in {turns} guesses!')
             break
         print()
         guess = input("Guess a letter: ")
+        guesses += guess
+        if guess not in random_word:
+            turns -= 1
+            print(f'Not quite. You have {turns} guesses left.')
+            if turns == 0:
+                print("Game over")
 
 
 if __name__ == "__main__":
