dae305d25d85c32864236f62cfe776a8cf914c95
Stephen Reish <spreish@ncsu.edu>
2022-04-28 09:27:31 -0400

    Started laying groundwork for automatic input capture

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,7 @@
 import random
 import time
+# from pynput.keyboard import Key, Controller
+# keyboard = Controller()
     
 
 def play_start():
@@ -196,38 +198,29 @@ def play_game(random_word):
     print()
     print(f'---')
     time.sleep(1)
-
     print(f'I am thinking of a word.')
     time.sleep(2)
     print(f'It has {len(random_word)} letters:')
     print()
-
     guesses = ' '
     turns = 8
-
     while turns > 0:
         failed = 0
-
         for letter in random_word:
             if letter in guesses:
                 print(letter, end=" ")
-
             else:
                 print("_", end=" ")
                 failed += 1
-
         if failed == 0:
             print(f'Good job! You guessed the word in {8 - turns} guesses! 😀')
             play_again()
-
         print()
         guess = input("Guess a letter... ")
         guesses += guess
-
         if guess not in random_word:
             turns -= 1
             print(f'Not quite. You have {turns} guesses left.')
-
             if turns == 0:
                 print(f'So close! The word was {random_word}.')
                 play_again()
